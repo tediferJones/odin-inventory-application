9eabcc09cc9553e64e1e8b733894b507341d13f9
ted <theo_drz@live.com>
2022-08-30 14:30:40 -0400

    finsihed views and controllers for home page, gameConsole_list, and gameConsole_detail

--- controllers/gameConsoleController.js
+++ controllers/gameConsoleController.js
@@ -1,16 +1,57 @@
+const async = require('async');
 const GameConsole = require('../models/gameConsole');
+const Game = require('../models/game');
+const Accessory = require('../models/accessory');
+const gameConsole = require('../models/gameConsole');
 
 //SPECIAL
 exports.index = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+  // res.send('NOT IMPLEMENTED: Site Home Page');
+  async.parallel({
+    gameConsole_count(callback) {
+      GameConsole.countDocuments({}, callback);
+    },
+    game_count(callback) {
+      Game.countDocuments({}, callback);
+    },
+    accessory_count(callback) {
+      Accessory.countDocuments({}, callback);
+    }
+  }, (err, results) => {
+    res.render('index', { title: "The Game Boy Store", error: err, data: results });
+  });
 };
 
+
 exports.gameConsole_list = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Game Console List');
+  // res.send('NOT IMPLEMENTED: Game Console List');
+  GameConsole.find({}, 'name')
+    .sort({title : 1})
+    .exec(function (err, gameConsole_list) {
+      if (err) { return next(err); }
+      res.render('gameConsole_list', { title: 'Game Console List', gameConsole_list })
+    })
 };
 
 exports.gameConsole_detail = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Game Console Detail');
+  // res.send('NOT IMPLEMENTED: Game Console Detail');
+  async.parallel({
+    gameConsole_info(callback) {
+      gameConsole.findById(req.params.id)
+      .exec(callback);
+    }
+  }, (err, results) => {
+    if (err) { return next(err); }
+
+    if (results.gameConsole_info == null) {
+      // No Results found
+      const err = new Error('Game Console Not Found');
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render('gameConsole_detail', {title: 'Game Console Details', gameConsole_info: results.gameConsole_info })
+  })
 };
 
 exports.gameConsole_create_get = (req, res, next) => {
--- models/gameConsole.js
+++ models/gameConsole.js
@@ -6,6 +6,7 @@ var ConsoleSchema = new Schema (
   {
     name: {type: String, required: true, maxLength: 100},
     description: {type: String, required: true},
+    // CONSIDER ADDING A "CONDITION" ATTR, could use simple scale of 1/10 - 10/10
     price: {type: Number, required: true},
     instock: {type: Number, required: true}
   }
@@ -15,7 +16,7 @@ var ConsoleSchema = new Schema (
 ConsoleSchema
 .virtual('url')
 .get(function () {
-  return '/catalog/console/' + this._id;
+  return '/catalog/gameConsole/' + this._id;
 });
 
 module.exports = mongoose.model('Console', ConsoleSchema);
